2862d791435085af2f0e52433f33b546385833a1
sanjaypandit1999 <sanjay7001532411@gmail.com>
2021-09-29 23:34:49 +0530

    ADDED: Insert data into employee_payroll table using JDBC

--- src/main/java/com/bridgelabz/EmployeePayRollService.java
+++ src/main/java/com/bridgelabz/EmployeePayRollService.java
@@ -1,6 +1,7 @@
 package com.bridgelabz;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -9,12 +10,14 @@ public class EmployeePayRollService {
         DbConfig dbConfig = new DbConfig();
         Connection connection = null;
         connection = dbConfig.getConnect();
+        PreparedStatement preparedStatement;
 
         String CREATE_DATABASE = "CREATE DATABASE payroll_service";
         String CREATE_TABLE = "Create Table employee_payroll (id int NOT NULL AUTO_INCREMENT, name VARCHAR(150) NOT NULL,salary Double NOT NULL,start DATE NOT NULL, PRIMARY KEY (id))";
+        String INSERT_DATA = "insert into employee_payroll (name,salary,start) values ('Sanjay',30000,'2021-08-15'),('Aviral',30000,'2021-06-21'),('Prem',40000,'2021-03-22')";
         try {
             Statement statement = connection.createStatement();
-            statement.execute(CREATE_TABLE);
+            statement.execute(INSERT_DATA);
             System.out.println("Query Executed");
         } catch (SQLException e) {
             e.printStackTrace();
