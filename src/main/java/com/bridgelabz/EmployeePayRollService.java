32799057af807525889e7f1982e05fe7bde8b089
sanjaypandit1999 <sanjay7001532411@gmail.com>
2021-09-29 22:40:54 +0530

    ADDED: Create employee_payroll table using JDBC

--- src/main/java/com/bridgelabz/EmployeePayRollService.java
+++ src/main/java/com/bridgelabz/EmployeePayRollService.java
@@ -10,10 +10,11 @@ public class EmployeePayRollService {
         Connection connection = null;
         connection = dbConfig.getConnect();
 
-        String CREAT_DATABASE = "CREATE DATABASE payroll_service";
+        String CREATE_DATABASE = "CREATE DATABASE payroll_service";
+        String CREATE_TABLE = "Create Table employee_payroll (id int NOT NULL AUTO_INCREMENT, name VARCHAR(150) NOT NULL,salary Double NOT NULL,start DATE NOT NULL, PRIMARY KEY (id))";
         try {
             Statement statement = connection.createStatement();
-            statement.execute(CREAT_DATABASE);
+            statement.execute(CREATE_TABLE);
             System.out.println("Query Executed");
         } catch (SQLException e) {
             e.printStackTrace();
